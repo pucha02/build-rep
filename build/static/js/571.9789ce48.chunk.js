af32ee7c9754a3f33e536b337d3ef50ecf7c65ef
pucha02 <puchko.v.keykey@gmail.com>
2024-11-01 19:48:26 +0200

    Add files via upload

---
+++ build/static/js/571.9789ce48.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrlc=self.webpackChunkrlc||[]).push([[571],{8571:(e,c,a)=>{a.d(c,{A:()=>s});a(5043);const s=a.p+"static/media/Group12.fca6e47aa36e41cb6e4b8b3eba43d872.svg"}}]);
+//# sourceMappingURL=571.9789ce48.chunk.js.map
