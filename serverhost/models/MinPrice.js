3b80eb08c45147c0f5cbeec18bf1605501e0048c
pucha02 <puchko.v.keykey@gmail.com>
2024-11-21 17:18:13 +0200

    Delete serverhost directory

--- serverhost/models/MinPrice.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const priceSchema = new mongoose.Schema({
-    minPrice: { type: String }
-});
-
-const MinPrice = mongoose.model('MinPrice', priceSchema);
-
-export default MinPrice;
