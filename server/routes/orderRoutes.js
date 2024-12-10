395f59ad496734bec7be1b2df57d554a885e655c
pucha02 <puchko.v.keykey@gmail.com>
2024-12-10 16:17:46 +0200

    Delete server directory

--- server/routes/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { registerOrder } from '../controllers/orderController.js';
-
-const router = express.Router();
-
-router.post('/register-order', registerOrder);
-
-export default router;
