40e57f50e0754bde1529a755782f65b4fdecc29e
pucha02 <puchko.v.keykey@gmail.com>
2025-01-29 15:39:54 +0200

    Delete server directory

--- server/MongooseModels/Cart.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const cartSchema = new mongoose.Schema({
-    cart: {type: Array} 
-});
-
-const Cart = mongoose.model('Cart', cartSchema);
-
-export default Cart;
--- server/routes/colorRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getColors } from '../controllers/colorController.js';
-
-const router = express.Router();
-
-router.get('/get-colors', getColors);
-
-export default router;
