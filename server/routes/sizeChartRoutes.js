65961bd86ac39a10359c6d97809df91cea357f9c
pucha02 <puchko.v.keykey@gmail.com>
2025-02-12 14:32:57 +0200

    Delete server directory

--- server/routes/sizeChartRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getSizeChart } from '../controllers/sizeChartController.js';
-
-const router = express.Router();
-
-router.get('/size-chart/:id', getSizeChart);
-
-export default router;
