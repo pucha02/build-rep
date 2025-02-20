6ffbbc201cd54a4eb51dc14d900c5ec211c046c0
pucha02 <puchko.v.keykey@gmail.com>
2025-02-20 10:11:09 +0200

    Delete server directory

--- server/routes/categoryRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getCategories } from '../controllers/categoryController.js';
-
-const router = express.Router();
-
-router.get('/get-categories', getCategories);
-
-export default router;
