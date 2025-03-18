90cdb04d66706e61037bc07953e9f0c74ed56567
pucha02 <puchko.v.keykey@gmail.com>
2025-03-18 10:26:20 +0200

    Delete server directory

--- server/routes/messageRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getMessages } from '../controllers/messageController.js';
-
-const router = express.Router();
-
-router.get('/get-messages', getMessages);
-
-export default router;
