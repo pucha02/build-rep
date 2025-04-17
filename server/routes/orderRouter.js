66459343316a84c3be15e6badcf1c5c9fc070445
pucha02 <puchko.v.keykey@gmail.com>
2025-04-17 10:49:09 +0300

    Delete server directory

--- server/routes/orderRouter.js
+++
@@ -1,7 +0,0 @@
-const Router = require("express");
-const router = new Router();
-const orderController = require("../controllers/orderController");
-router.post("/", orderController.create);
-router.get("/orders", orderController.getAllOrders);
-
-module.exports = router;
